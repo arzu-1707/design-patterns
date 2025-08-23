66f6ba234881f68bea24ffdf3ab4c7761f0acc40
arzu <ahmed.arzu95@gmail.com>
2025-08-23 13:32:31 +0400

    Double Checked Locking Singleton is created

---
+++ src/singleton/DoubleCheckedLockingSingleton.java
@@ -0,0 +1,21 @@
+package singleton;
+
+public class DoubleCheckedLockingSingleton {
+    private static DoubleCheckedLockingSingleton instance ;
+
+    private DoubleCheckedLockingSingleton(){
+
+    }
+
+    public static DoubleCheckedLockingSingleton getInstance(){
+
+        if (instance == null) {
+            synchronized (DoubleCheckedLockingSingleton.class) {
+                if (instance == null) {
+                    instance = new DoubleCheckedLockingSingleton();
+                }
+            }
+        }
+        return instance;
+    }
+}
